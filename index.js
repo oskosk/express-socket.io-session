259e8d37f80be674890cecdcb97c64dbc83d1d34
osk <oskosk@gmail.com>
2015-01-25 16:34:22 -0300

    Added parameter for own cookie-parser instance

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-var cookieParser = require('cookie-parser')();
+var cookieparser = require('cookie-parser');
 var debug = require("debug")("express-socket.io-session");
-/*
- * Inspiration
- * ============
- *
- * [socket.io and express 4 sessions]
- * http://stackoverflow.com/questions/23494016/socket-io-and-express-4-sessions
- *
- * [Socket.io 1.0.5 : How to save session variables?]
- * http://stackoverflow.com/questions/24290699/socket-io-1-0-5-how-to-save-session-variables/24380110#24380110
- *
- * sesion.socket.io
- * ----------------
- * I don't like this one because you can not reuse
- * https://www.npmjs.org/package/session.socket.io
- */
 
 // The express session object will be set
 // in socket.handskake.session.
@@ -22,22 +7,32 @@ var debug = require("debug")("express-socket.io-session");
 /**
  * Returns a middleware function that acts on socket.handshake
  *
- * @param {Function} an express-session middleware function
+ * @param {Function} an express-session middleware function to reuse with io.use
+ * @param {Function} an express-session middleware function to reuse with express-session
  */
-module.exports = function(session) {
+module.exports = function(expressSessionMiddleware, cookieParserMiddleware) {
+  var cookieParserMiddleware,
+    socketIoSharedSessionMiddleware;
+
+  if (typeof cookieParserMiddleware === 'undefined') {
+    debug("No cookie-parser instance passed as argument");
+    debug("Creating a cookie-parser instance with default values")
+    cookieParserMiddleware = cookieparser();
+  }
   debug("Creating socket.io middleware");
-  return function(socket, next) {
+  var socketIoSharedSessionMiddleware = function(socket, next) {
     var req = socket.handshake;
     var res = {};
     //Parse session cookie
-    cookieParser(req, res, function(err) {
+    cookieParserMiddleware(req, res, function(err) {
       if (err) {
         debug("cookieParser errored");
         return next(err);
       }
-      session(req, res, function(req, res) {
+      expressSessionMiddleware(req, res, function(req, res) {
         next();
       });
     });
   };
+  return socketIoSharedSessionMiddleware;
 };
