335b980e89bb23c6b1d165b4827fe2ad44a2908c
osk <oskosk@gmail.com>
2015-07-03 14:52:20 -0300

    Added autoSave feature. experimental behaviour;

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var cookieparser = require('cookie-parser');
 var debug = require("debug")("express-socket.io-session");
-
+var crc = require("crc").crc32;
 // The express session object will be set
 // in socket.handskake.session.
 
@@ -16,25 +16,45 @@ var debug = require("debug")("express-socket.io-session");
 module.exports = function(expressSessionMiddleware, cookieParserMiddleware, options) {
   var socketIoSharedSessionMiddleware;
 
+  // Accept options as second argument if only 2 parameters passed
+  if (arguments.length == 2 && typeof cookieParserMiddleware === 'object') {
+    options = cookieParserMiddleware;
+    cookieParserMiddleware = undefined;
+  }
+
   if (typeof cookieParserMiddleware === 'undefined') {
     debug("No cookie-parser instance passed as argument. Creating a cookie-parser " +
       "instance with default values");
     cookieParserMiddleware = cookieparser();
   }
   options = options || {};
+  var saveUninitializedSession = options.saveUninitialized;
   debug("Creating socket.io middleware");
 
   socketIoSharedSessionMiddleware = function(socket, next) {
     var req = socket.handshake;
-    var res = {};
-    var _on = socket.on;
+    var res = {
+      end: function() {}
+    };
+    // originalHash, savedHash, originalId, cookieId
+    // are variables present for replicating express-session autoSaving behavioiur
+    var originalHash, savedHash;
+    var originalId;
+    var cookieId;
+    var _onevent = socket.onevent;
     // Override socket.on if autoSave = true; 
     if (options.autoSave === true) {
-      socket.on = function() {
+      debug("Using autoSave feature. express-session middleware will be called on every event received")
+      socket.onevent = function() {
+        debug("Executing socket.onevent monkeypatched by express-socket.io-session");
         var _args = arguments;
-        expressSessionMiddleware(req, res, function(req, res) {
-          _on.apply(socket, _args);
-        });
+        originalHash = savedHash = hash(req.session);
+        cookieId = req.sessionID;
+        originalId = req.sessionID;
+        _onevent.apply(socket, _args);
+        if (shouldSave(req)) {
+          req.session.save()
+        }
       };
     }
     //Parse session cookie
@@ -47,6 +67,55 @@ module.exports = function(expressSessionMiddleware, cookieParserMiddleware, opti
         next();
       });
     });
+    /*
+     * These functions hash, isModified, isSaved, shouldSave
+     * and shouldDestroy are canibalized from express-session
+     * in order to this module being able to comply with the autoSave options.
+     */
+
+    /**
+     * Hash the given `sess` object omitting changes to `.cookie`.
+     *
+     * @param {Object} sess
+     * @return {String}
+     * @private
+     */
+
+    function hash(sess) {
+      return crc(JSON.stringify(sess, function(key, val) {
+        if (key !== 'cookie') {
+          return val;
+        }
+      }));
+    }
+
+    // check if session has been modified
+    function isModified(sess) {
+      return originalId !== sess.id || originalHash !== hash(sess);
+    }
+
+    // check if session has been saved
+    function isSaved(sess) {
+      return originalId === sess.id && savedHash === hash(sess);
+    }
+
+    // determine if session should be destroyed
+    function shouldDestroy(req) {
+      return req.sessionID && unsetDestroy && req.session == null;
+    }
+
+    // determine if session should be saved to store
+    function shouldSave(req) {
+      // cannot set cookie without a session ID
+      if (typeof req.sessionID !== 'string') {
+        debug('session ignored because of bogus req.sessionID %o', req.sessionID);
+        return false;
+      }
+
+      return !saveUninitializedSession && cookieId !== req.sessionID ? isModified(req.session) : !isSaved(req.session)
+    }
   };
+
+
   return socketIoSharedSessionMiddleware;
 };
